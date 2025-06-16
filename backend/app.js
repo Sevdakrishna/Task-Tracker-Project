a442a2143abd250fb99bd4888169ccfc5e8b02ee
Sevdakrishna <sevdakrishna@gmail.com>
2025-06-16 11:23:37 +0530

    Backend Complete

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,19 @@ const authRoutes = require('./routes/authRoutes');
 const { createUserTable } = require('./models/userModel');
 const { createTaskTable } = require('./models/taskModel')
 
+const taskRoutes = require('./routes/taskRoutes');
+const adminRoutes = require('./routes/adminRoutes')
+
+
+
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/admin', adminRoutes)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
